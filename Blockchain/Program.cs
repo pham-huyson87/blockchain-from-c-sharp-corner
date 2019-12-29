41a808b72e5b1cdb5f87eb965e43a7cb945ae95b
pham-huyson87 <pham.huyson87@hotmail.com>
2019-12-28 22:12:47 -0500

    - Add a basic Blockchain

--- Blockchain/Program.cs
+++ Blockchain/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using BlockchainTest.Models;
+using Newtonsoft.Json;
+using System;
 
-namespace Blockchain
+namespace BlockchainTest
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Blockchain phillyCoin = new Blockchain();
+            phillyCoin.AddBlock(new Block(DateTime.Now, null, "{sender:Henry,receiver:MaHesh,amount:10}"));
+            phillyCoin.AddBlock(new Block(DateTime.Now, null, "{sender:MaHesh,receiver:Henry,amount:5}"));
+            phillyCoin.AddBlock(new Block(DateTime.Now, null, "{sender:Mahesh,receiver:Henry,amount:5}"));
+
+            Console.WriteLine(JsonConvert.SerializeObject(phillyCoin, Formatting.Indented));
+
+
+
+            // Test blockchain validation
+
+            Console.WriteLine($"Is Chain Valid: {phillyCoin.IsValid()}");
+
+            Console.WriteLine($"Update amount to 1000");
+            phillyCoin.Chain[1].Data = "{sender:Henry,receiver:MaHesh,amount:1000}";
+            phillyCoin.Chain[1].Hash = phillyCoin.Chain[1].CalculateHash();
+
+            Console.WriteLine($"Is Chain Valid: {phillyCoin.IsValid()}");
         }
     }
 }
